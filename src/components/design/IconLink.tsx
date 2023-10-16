8c6a2c8448fb203cc3f8b6253be20e13bc48a989
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-10-16 22:59:44 +0500

    added: icon link component

---
+++ src/components/design/IconLink.tsx
@@ -0,0 +1,22 @@
+import { FlexProps } from "@chakra-ui/react";
+import { ReactNode } from "react";
+import Link from "next/link";
+import IconText from "./IconText";
+
+export interface IconLinkProps  
+{
+    url: string
+    icon?: ReactNode
+    highlighted?: boolean
+    flexProps?: FlexProps
+    children: ReactNode
+}
+
+export default function IconLink (props: IconLinkProps) 
+{
+    return <Link href={props.url}>
+        <IconText icon={props.icon} highlighted={props.highlighted}>
+            {props.children}
+        </IconText>
+    </Link>
+}
