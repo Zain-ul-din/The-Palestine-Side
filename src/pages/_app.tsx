e8ccbcf91466213fb617abdb74b4057a409f3818
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-10-16 23:00:59 +0500

    added: MainLayout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import Header from '@/components/Header';
+import MainLayout from '@/Layout/MainLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider>
-            <Header />            
-            <Component {...pageProps} />
+            <MainLayout>
+                <Component {...pageProps} />
+            </MainLayout>
         </ChakraProvider>        
     ); 
 }
