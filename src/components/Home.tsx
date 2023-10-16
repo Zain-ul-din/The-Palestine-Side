fe34b08fc3131b83ff1873260fd27539e1b68f9a
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-10-16 22:58:58 +0500

    init: home content

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import { Flex, Heading } from "@chakra-ui/react";
+
+export default function Home () 
+{
+    return <Flex width={'100%'}>
+        <Flex width={'100%'} maxW={'1420px'} m={'0 auto'} p={4}>
+            <Heading>
+                Table of Content
+            </Heading>
+        </Flex>
+    </Flex>
+}
