4027e182b13bbe3da0fc7496f7576ac17044083e
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-10-17 23:45:29 +0500

    added: Content layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,34 @@
+import TableOfContent from "@/types/TableOfContent";
 import { Flex, Heading } from "@chakra-ui/react";
+import IconLink from "./design/IconLink";
+import { ROUTES } from "@/lib/constant";
+import ContentLayout from "@/Layout/ContentLayout";
 
-export default function Home () 
-{
-    return <Flex width={'100%'}>
-        <Flex width={'100%'} maxW={'1420px'} m={'0 auto'} p={4}>
-            <Heading>
-                Table of Content
-            </Heading>
-        </Flex>
-    </Flex>
+interface HomeProps {
+    tableOfContent: TableOfContent
 }
+
+export default function Home ({ tableOfContent } : HomeProps) 
+{
+    return <ContentLayout gap={3}>
+        {/* table of content */}
+        <Heading>
+            Table of Content
+        </Heading>
+        <Flex ml={7} flexDir={'column'} gap={2}>
+            {tableOfContent && tableOfContent.map((t,i)=>{
+                return <IconLink
+                    key={i}
+                    url={`${ROUTES.Content}/${t}`}
+                    flexProps={{
+                        display: 'list-item',
+                        color: 'blue.400'
+                    }}
+                >
+                    {t}
+                </IconLink>
+            })}
+        </Flex>
+    </ContentLayout>
+}
+
