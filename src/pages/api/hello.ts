60c9eaedbc653a26d2a851a894b998ce531a5e7e
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-10-18 20:03:36 +0500

    removed: api end points #2
    since we can run server side code inside next js build in function
    'getStaticProps' and 'getStaticPaths' no need to add restful apis
    endpoint anymore

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,10 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiResponse, NextApiRequest } from "next";
 
-type Data = {
-    name: string;
-};
-
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    res.status(200).json({ name: 'John Doe' });
+export default function handler(req: NextApiRequest, res: NextApiResponse) 
+{
+    res.status(200).json({ msg: 'hello world' })
 }
