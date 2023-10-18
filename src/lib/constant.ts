b0a991accb6c40cb07f4e0e2ae7473a06dc5f809
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-10-18 20:00:24 +0500

    removed: restful apis end points #2

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,8 +1,3 @@
-export const API_ENDPOINTS = {
-    TableOfContent: `${process.env.NEXT_PUBLIC_HOST_NAME}/api/table_of_content`,
-    Content: `${process.env.NEXT_PUBLIC_HOST_NAME}/api/content`
-}
-
 export const ROUTES = {
     Home: '/',
     Videos: '/videos',
