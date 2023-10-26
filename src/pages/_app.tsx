ee3b79e8db04783e5e13add0211cb2144c632c72
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-10-26 20:15:11 +0500

    added: chakra ui theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from '@/styles/theme';
 import MainLayout from '@/Layout/MainLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <MainLayout>
                 <Component {...pageProps} />
             </MainLayout>
         </ChakraProvider>        
     ); 
 }
+
