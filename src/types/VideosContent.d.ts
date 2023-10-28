cbd836d416de030499c625226828e7c45ecaa854
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-10-28 12:28:10 +0500

    added: Country type #2

--- src/types/VideosContent.d.ts
+++ src/types/VideosContent.d.ts
@@ -1,4 +1,6 @@
+import Country from "./Country";
+
 export default interface VideosContent 
 {
-    videos: Array<{ title: string, url: string }>
+    videos: Array<{ title: string, url: string, country?: Country }>
 }
