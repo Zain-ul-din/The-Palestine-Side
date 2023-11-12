d3f5136e0354734dbfc90597e6703a9c0105e75f
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-11-13 01:17:50 +0500

    fixed: FOUC (Flash of Unstyled Content)  issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,25 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { CSSReset, ChakraProvider } from '@chakra-ui/react';
 import theme from '@/styles/theme';
 import MainLayout from '@/Layout/MainLayout';
-import { useRouter } from 'next/router';
+import { motion } from 'framer-motion';
 
 export default function App({ Component, pageProps }: AppProps) {
     
     return (
         <ChakraProvider theme={theme}>
             <MainLayout>
-                <Component {...pageProps} />
+                {/* Prevent FOUC (Flash of Unstyled Content)  */}
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.1, delay: 0.3 }}
+                >
+                    <Component {...pageProps} />
+                </motion.div>
             </MainLayout>
+            <CSSReset />
         </ChakraProvider>        
     ); 
 }
