6ff722854b77c8e4c55ac025dcb9328af7175020
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-11-12 18:48:29 +0500

    added: firebase logs

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,11 +6,11 @@ export default function Home ()
     const { colorMode } = useColorMode()
     
     return <ContentLayout gap={3} p={2}>
-        <Center my={3} flexDir={'column'} gap={2}>
-            <Heading textAlign={'center'} color={colorMode == "light" ? 'green.700' : 'green.300'} fontWeight={'extrabold'}>
-                The Palestine Side
+        <Center my={4} flexDir={'column'} gap={2}>
+            <Heading textAlign={'center'} color={colorMode == "light" ? 'green.600' : 'green.300'} fontWeight={'extrabold'}>
+                {`"The Palestine Side"`}
             </Heading>
-            <Heading color={colorMode == "light" ? 'red.600' : 'red.300'} textAlign={'center'} fontSize={'xl'}>
+            <Heading color={colorMode == "light" ? 'gray.800' : 'gray.50'} textAlign={'center'} fontSize={'xl'}>
                 of Palestine-Israel Conflict
             </Heading>
         </Center>
