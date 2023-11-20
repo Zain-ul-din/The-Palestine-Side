817ca19e18aa93bb180543e61fccd3030b78bb60
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-11-20 21:24:45 +0500

    added: awesome palestine page

---
+++ src/components/AwesomePalestine.tsx
@@ -0,0 +1,16 @@
+import { Center, Heading } from "@chakra-ui/react";
+
+export default function AwesomePalestine () {
+    
+    return <>
+        <Center>
+            <Heading
+                fontSize={'2xl'}
+                my={2}
+                textAlign={'center'}
+            >
+                A curated list of Palestine and Palestinian-Israeli conflict resources.
+            </Heading>
+        </Center>
+    </>
+}
---
+++ src/pages/awesome-palestine/index.tsx
@@ -0,0 +1,41 @@
+import ContentLayout from "@/Layout/ContentLayout";
+import AwesomePalestine from "@/components/AwesomePalestine";
+import Head from "@/components/Head";
+import MarkDownContent from "@/components/MarkDownContent";
+import { EXTERNAL_API_LINKS } from "@/lib/constant";
+import { GetStaticPropsContext } from "next";
+
+export default function Page ({ content } : { content: string}) {
+    return <>
+        <Head 
+            title="Awesome Palestine"
+            description="A curated list of Palestine and Palestinian-Israeli conflict resources."
+        />
+        <main>
+            <ContentLayout p={2}>
+                <AwesomePalestine />
+                <MarkDownContent
+                    markDownConfig={{
+                        linkRedirect: '_self',
+                        useHashRouting: true
+                    }}
+                >
+                    {content}
+                </MarkDownContent>
+            </ContentLayout>
+        </main>
+    </>
+}
+
+export async function getStaticProps(context: GetStaticPropsContext)
+{
+    const content = await (await 
+        fetch(EXTERNAL_API_LINKS.AWESOME_PALESTINE_README)
+    ).text()
+    
+    return {
+        props: {
+            content
+        }
+    }
+}
