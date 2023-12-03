5eb149417cd7944881b6edaf735628b426e9e06f
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-12-03 12:04:41 +0500

    added: PWA splash_screens

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,16 +9,16 @@ export default function App({ Component, pageProps }: AppProps) {
     
     return (
         <ChakraProvider theme={theme}>
-            <MainLayout>
-                {/* Prevent FOUC (Flash of Unstyled Content)  */}
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 0.1, delay: 0.3 }}
-                >
+            {/* Prevent FOUC (Flash of Unstyled Content)  */}
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.3 }}
+            >
+                <MainLayout>
                     <Component {...pageProps} />
-                </motion.div>
-            </MainLayout>
+                </MainLayout>
+            </motion.div>
             <CSSReset />
         </ChakraProvider>        
     ); 
